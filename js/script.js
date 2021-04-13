3cb784efc33cdd5e47594a940a6bbb89f1222a5f
Leon <leon@famnabuurs.nl>
2021-04-13 09:45:01 +0200

    opdracht afgerond

---
+++ js/script.js
@@ -0,0 +1,19 @@
+console.log("Oefening om pizza te maken");
+
+const maakpizza = function() {
+    console.log("maak deeg");
+    console.log("maak vulling");
+    console.log("bak de pizza");
+}
+const maaksushi = function() {
+    console.log("maak zeewierbladeren schoon");
+    console.log("maak zalm schoon");
+    console.log("vouw in elkaar");
+}
+
+maaksushi();
+maaksushi();
+maaksushi();
+maakpizza();
+maakpizza();
+
